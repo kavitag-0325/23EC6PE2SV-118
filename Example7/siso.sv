5d114fe2364e1f2acebe41ac9d01e713bf672a6c
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-02-22 23:22:27 +0530

    Update siso.sv

--- Example7/siso.sv
+++ Example7/siso.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : siso.sv
-//Author     : Krishna Gupta/1BM23EC123
+//Author     : Kavita/1BM23EC118
 //Created    : 2026-01-24
 //Module     : siso
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
