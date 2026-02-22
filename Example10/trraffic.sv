ef042a288d210a1107ab9f5f5c29ff5c8bef56d7
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-02-22 23:17:15 +0530

    Update trraffic.sv

--- Example10/trraffic.sv
+++ Example10/trraffic.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : traffic.sv
-//Author     : Krishna Gupta/1BM23EC123
+//Author     : Kavita/1BM23EC118
 //Created    : 2026-01-28
 //Module     : traffic
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
