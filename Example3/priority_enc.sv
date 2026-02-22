9b46744c1b2c5de748dc31ed3ab7251f2b5ef965
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-02-22 23:19:18 +0530

    Update priority_enc.sv

--- Example3/priority_enc.sv
+++ Example3/priority_enc.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : priority_enc.sv
-//Author     : Krishna Gupta/1BM23EC123
+//Author     : Kavita/1BM23EC118
 //Created    : 2026-01-23
 //Module     : priority_enc
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
