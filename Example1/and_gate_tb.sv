7ca1e511a8a73b3f61083d6d23e25fc81da7ebc7
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-02-22 23:16:34 +0530

    Update and_gate_tb.sv

--- Example1/and_gate_tb.sv
+++ Example1/and_gate_tb.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : and_gate_tb.sv
-//Author     : Krishna Gupta/1BM23EC123
+//Author     : Kavita/1BM23EC118
 //Created    : 2026-01-20
 //Module     : tb
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
