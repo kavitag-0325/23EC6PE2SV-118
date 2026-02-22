94a2e392d1f1d835867203afd9f80ef6f6b4674d
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-02-22 23:28:45 +0530

    Update dual_port_ram_tb.sv

--- AAT-EDA/Task5/dual_port_ram_tb.sv
+++ AAT-EDA/Task5/dual_port_ram_tb.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : dual_port_ram_tb.sv
-//Author     : Krishna Gupta/1BM23EC123
+//Author     : Kavita/1BM23EC118
 //Created    : 2026-01-29
 //Module     : tb
 //Project    : SystemVerilog and Verification (23EC6PE2SV)
